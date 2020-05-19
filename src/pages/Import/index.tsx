f46099767422c125f21333b3ad6404ff8b140891
Tenclar <tenclarvalus@gmail.com>
2020-05-18 20:27:25 -0500

    importacao de arquivo

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
     // TODO
+    if (!uploadedFiles.length) return;
+
+    const fileUp = uploadedFiles[0];
+    data.append('file', fileUp.file, fileUp.name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
     // TODO
+    const ufiles = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+    setUploadedFiles(ufiles);
   }
 
   return (
